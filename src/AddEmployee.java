a35e841c20c5174958512838d277a60afbea4274
FoolyxCooly <82619286+FoolyxCooly@users.noreply.github.com>
2024-04-28 19:59:39 -0400

    yes

--- src/AddEmployee.java
+++ src/AddEmployee.java
@@ -3,11 +3,11 @@ public class AddEmployee {
     String url = "jdbc:mysql://localhost:3306/employeedata";
     String user = "root";
     String password = "";
-
+ 
     public void setEmployee(int empID, String empfName, String emplName, String empEmail, String empHireDate, int empSalary){
         String sqlcom1=
-        "INSERT INTO employees (empid,Fname,Lname,email,HireDate,Salaray)"+ 
-        "VALUES ('"+empfName +"', '"+emplName+", '"+empEmail+", '"+empHireDate+", '"+empSalary+"'); ";
+        "INSERT INTO employees (Fname, Lname, email, HireDate, Salary, empid)"+ 
+        "VALUES('"+empfName+"', '"+emplName+"', '"+empEmail+"', '"+empHireDate+"', "+empSalary+", "+empID+ "); ";
         try (Connection myConn = DriverManager.getConnection(url, user, password)) {
             Statement myStmt = myConn.createStatement();
 
