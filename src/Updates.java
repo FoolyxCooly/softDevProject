27a565390d466927b2333cdebd141d255a699169
YummyFrog <122713162+YummyFrog@users.noreply.github.com>
2024-04-28 17:03:17 -0400

    Final Updates.java
    This should be completed.

--- src/Updates.java
+++ src/Updates.java
@@ -1,4 +1,6 @@
 import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Updates {
     String url = "jdbc:mysql://localhost:3306/employeedata";
@@ -21,14 +23,12 @@ public class Updates {
         }
     }
     public void updateEmpName(int empID, String empfName, String emplName){
-        String sqlcom1="UPDATE employees"+
-        "SET Fname = "+ empfName +" , Lname = "+emplName+
-        "WHERE empid = "+empID+" ; ";
+        String sqlcom1 = "UPDATE employees " +
+                         "SET Fname = '" + empfName + "', Lname = '" + emplName + "' " +
+                         "WHERE empid = " + empID + ";";
         try (Connection myConn = DriverManager.getConnection(url, user, password)) {
             Statement myStmt = myConn.createStatement();
-
             myStmt.executeUpdate(sqlcom1);
-        
             myConn.close();
         } catch (Exception e) {
             System.out.println("ERROR " + e.getLocalizedMessage());
@@ -36,14 +36,14 @@ public class Updates {
         }
     }
     public void updateEmpEmail(int empID, String empEmail){
-        String sqlcom1="UPDATE employees"+
-        "SET email = "+ empEmail +
-        "WHERE empid = "+empID+" ; ";
+        String sqlcom1 = "UPDATE employees " +
+                         "SET email = '" + empEmail + "' " +
+                         "WHERE empid = " + empID + ";";
         try (Connection myConn = DriverManager.getConnection(url, user, password)) {
             Statement myStmt = myConn.createStatement();
-
+    
             myStmt.executeUpdate(sqlcom1);
-        
+    
             myConn.close();
         } catch (Exception e) {
             System.out.println("ERROR " + e.getLocalizedMessage());
@@ -51,33 +51,48 @@ public class Updates {
         }
     }
     public void updateEmpSalary(int empID, String empSalary){
-        String sqlcom1="UPDATE employees"+
-        "SET Salary = "+ empSalary +
-        "WHERE empid = "+empID+" ; ";
+        String sqlcom1 = "UPDATE employees " +
+                         "SET Salary = '" + empSalary + "' " +
+                         "WHERE empid = " + empID + ";";
         try (Connection myConn = DriverManager.getConnection(url, user, password)) {
             Statement myStmt = myConn.createStatement();
-
+    
             myStmt.executeUpdate(sqlcom1);
-        
+    
             myConn.close();
         } catch (Exception e) {
             System.out.println("ERROR " + e.getLocalizedMessage());
         } finally {
         }
     }
-    public void updateEmpHireDate(int empID, String empHireDate){
-        String sqlcom1="UPDATE employees"+
-        "SET HireDate = "+ empHireDate +
-        "WHERE empid = "+empID+" ; ";
-        try (Connection myConn = DriverManager.getConnection(url, user, password)) {
-            Statement myStmt = myConn.createStatement();
-
-            myStmt.executeUpdate(sqlcom1);
-        
-            myConn.close();
-        } catch (Exception e) {
-            System.out.println("ERROR " + e.getLocalizedMessage());
-        } finally {
+    
+    public void updateEmpHireDate(int empID, String empHireDate) {
+        if (isValidDateFormat(empHireDate)) {
+            String sqlcom1 = "UPDATE employees " +
+                             "SET HireDate = '" + empHireDate + "' " +
+                             "WHERE empid = " + empID + ";";
+            try (Connection myConn = DriverManager.getConnection(url, user, password)) {
+                Statement myStmt = myConn.createStatement();
+    
+                myStmt.executeUpdate(sqlcom1);
+    
+                myConn.close();
+            } catch (Exception e) {
+                System.out.println("ERROR " + e.getLocalizedMessage());
+            }
+        } else {
+            System.out.println("Invalid date format. Please enter the date in the format YYYY-MM-DD.");
         }
     }
+
+    private boolean isValidDateFormat(String date) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        dateFormat.setLenient(false);
+        try {
+            dateFormat.parse(date);
+            return true;
+        } catch (ParseException e) {
+            return false;
+        }
+    }  
 }
